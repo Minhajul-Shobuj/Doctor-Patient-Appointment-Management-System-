6f833d77767de84e6dc6d1c7a7e52c6a6e2a5f14
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-06-28 01:16:10 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
+import router from "./app/routes";
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// application routes
+app.use("/api/v1", router);
+
+// const test = async (req: Request, res: Response) => {
+//   const a = 10;
+//   res.send(a);
+// };
+
+// app.get('/', test);
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
